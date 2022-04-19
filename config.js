500749f7002efb6e29131c78c3f109d82b2d6dc7
KazuKi <100437797+Jiplonk@users.noreply.github.com>
2022-04-19 08:01:16 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-global.owner = ['6288217843188'] // Put your number here
+global.owner = ['6283113989278'] // Put your number here
 global.mods = [] // Want some help?
 global.prems = [] // Premium user has unlimited limit
 global.APIs = { // API Prefix
@@ -42,12 +42,12 @@ global.APIKeys = { // APIKey Here
 
 // Sticker WM
 global.stiker_wait = 'Stiker sedang dibuat'
-global.packname = 'stickerby'
-global.author = `Reza Temayang`
-global.wm = 'Reza Temayang'
+global.packname = 'stickerbyKazuKi'
+global.author = `KazuKiGans`
+global.wm = 'KazuKiGans'
 global.watermark = wm
 global.media = 'https://telegra.ph/file/72168c6f1aaa90315c369.jpg'
-global.instagram = 'https://www.instagram.com/reteam.id/'
+global.instagram = 'https://www.instagram.com/jiplonk3dt2'
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=crafts-logo&doScale=true&scaleWidth=500&scaleHeight=500&fontsize=100&fillTextType=0&backgroundColor=%23401620&text='
 global.wait = '_*tunggu sedang di proses...*_'
 global.eror = '_*Server Error*_'
